4ed93011e7f854daea6541eed15d24bdc42b0590
Brent Schartung <bschartung@nvidia.com>
2023-04-10 20:44:43 +0000

    Fix adding sessions to hashtable. Fix root pipe. Fix SNAT actions. Write MAC addr. Improve cpu-rss logging.

--- main.c
+++ main.c
@@ -35,6 +35,8 @@ DOCA_LOG_REGISTER(HAIRPIN_MESH_EX);
 
 #define MAX_HT_ENTRIES 4096
 
+uint64_t default_entries_timeout_us = 100;
+
 typedef uint64_t session_key;
 
 struct session_entry
@@ -78,6 +80,12 @@ struct rte_hash_parameters session_ht_params = {
 	.extra_flag = 0, // see RTE_HASH_EXTRA_FLAGS_RW_CONCURRENCY_LF
 };
 
+const char *port_mac_addr[] = {
+	"da:6f:57:15:50:e4",
+	"7a:ed:c1:f4:fe:47",
+	"b6:ac:e9:bb:de:81"
+};
+
 ////////////////////////////////////////////////////////////////////////////////
 // Signal Handling
 
@@ -101,17 +109,58 @@ static void install_signal_handler(void)
 ////////////////////////////////////////////////////////////////////////////////
 // RSS Packet Processing
 
+
 static int
-log_packet(const struct rte_mbuf *packet)
+log_packet(uint32_t lcore_id, const struct rte_mbuf *packet)
 {
 	struct rte_ether_hdr *eth_hdr = rte_pktmbuf_mtod(packet, struct rte_ether_hdr *);
 	uint16_t ether_type = htons(eth_hdr->ether_type);
 
+	const char *ether_type_str = "UNKNOWN";
+	const char *proto = "UNKNOWN";
+	char src_addr[INET6_ADDRSTRLEN];
+	char dst_addr[INET6_ADDRSTRLEN];
+	uint16_t src_port = 0;
+	uint16_t dst_port = 0;
+	
 	if (ether_type == RTE_ETHER_TYPE_IPV4) {
-		DOCA_LOG_DBG("Received IPV4");
+		ether_type_str = "IPv4";
+		const struct rte_ipv4_hdr * ipv4_hdr = (struct rte_ipv4_hdr*)&eth_hdr[1];
+		inet_ntop(AF_INET, &ipv4_hdr->src_addr, src_addr, INET_ADDRSTRLEN);
+		inet_ntop(AF_INET, &ipv4_hdr->dst_addr, dst_addr, INET_ADDRSTRLEN);
+		if (ipv4_hdr->next_proto_id == IPPROTO_TCP) {
+			proto = "TCP";
+	    	const struct rte_tcp_hdr * tcp_hdr = (struct rte_tcp_hdr*)&ipv4_hdr[1];
+			src_port = htons(tcp_hdr->src_port);
+			dst_port = htons(tcp_hdr->dst_port);
+		} else if (ipv4_hdr->next_proto_id == IPPROTO_UDP) {
+			proto = "UDP";
+	    	const struct rte_udp_hdr * udp_hdr = (struct rte_udp_hdr*)&ipv4_hdr[1];
+			src_port = htons(udp_hdr->src_port);
+			dst_port = htons(udp_hdr->dst_port);
+		}
 	} else if (ether_type == RTE_ETHER_TYPE_IPV6) {
-		DOCA_LOG_DBG("received IPV6");
+		ether_type_str = "IPv6";
+		const struct rte_ipv6_hdr * ipv6_hdr = (struct rte_ipv6_hdr*)&eth_hdr[1];
+		inet_ntop(AF_INET6, &ipv6_hdr->src_addr, src_addr, INET6_ADDRSTRLEN);
+		inet_ntop(AF_INET6, &ipv6_hdr->dst_addr, dst_addr, INET6_ADDRSTRLEN);
+		if (ipv6_hdr->proto == IPPROTO_TCP) {
+			proto = "TCP";
+	    	const struct rte_tcp_hdr * tcp_hdr = (struct rte_tcp_hdr*)&ipv6_hdr[1];
+			src_port = htons(tcp_hdr->src_port);
+			dst_port = htons(tcp_hdr->dst_port);
+		} else if (ipv6_hdr->proto == IPPROTO_UDP) {
+			proto = "UDP";
+	    	const struct rte_udp_hdr * udp_hdr = (struct rte_udp_hdr*)&ipv6_hdr[1];
+			src_port = htons(udp_hdr->src_port);
+			dst_port = htons(udp_hdr->dst_port);
+		}
 	}
+	DOCA_LOG_INFO("L-Core %d: Received %s/%s %s:%d>%s:%d",
+		lcore_id,
+		ether_type_str, proto,
+		src_addr, src_port,
+		dst_addr, dst_port);
 
 	return 0;
 }
@@ -132,7 +181,7 @@ example_burst_rx(uint16_t port_id, uint16_t queue_id)
 
 		uint16_t nb_rx_packets = rte_eth_rx_burst(port_id, queue_id, rx_packets, MAX_RX_BURST_SIZE);
 		for (int i=0; i<nb_rx_packets; i++) {
-			log_packet(rx_packets[i]);
+			log_packet(rte_lcore_id(), rx_packets[i]);
 		}
 
 		double sec = (double)(rte_rdtsc() - t_start) * tsc_to_seconds;
@@ -201,14 +250,20 @@ apply_session_flow(
 	};
 
 	struct doca_flow_header_l4_port *match_l4_port = session->proto == IPPROTO_UDP ?
-		&match.outer.udp.l4_port : &match.outer.tcp.l4_port;
+		&match.outer.udp.l4_port : 
+		&match.outer.tcp.l4_port;
 
 	struct doca_flow_header_l4_port *action_l4_port = session->proto == IPPROTO_UDP ?
-		&match.outer.udp.l4_port : &match.outer.tcp.l4_port;
+		&actions[0].outer.udp.l4_port : 
+		&actions[0].outer.tcp.l4_port;
 
 	match_l4_port->src_port = session->src_port;
 	match_l4_port->dst_port = session->dst_port;
-	action_l4_port->src_port = session->src_port;
+	action_l4_port->src_port = session->snat_src_port;
+
+	memcpy(actions[0].outer.eth.dst_mac, 
+		port_mac_addr[session->egress_port], 
+		DOCA_ETHER_ADDR_LEN);
 
 	uint16_t rss_queues[] = {
 		dpdk_config->port_config.nb_ports + session->egress_port,
@@ -229,31 +284,56 @@ apply_session_flow(
 	if (res != DOCA_SUCCESS) {
 		DOCA_LOG_ERR("Port %u: session %lu: Failed to create pipe entry: %s", 
 			session->ingress_port, session->key, doca_get_error_string(res));
+		return res;
+	}
+	res = doca_flow_entries_process(port_flows->doca_port, 0, default_entries_timeout_us, 0);
+	if (res != DOCA_SUCCESS) {
+		DOCA_LOG_ERR("Port %u: session %lu: Failed to process pipe entry: %s", 
+			session->ingress_port, session->key, doca_get_error_string(res));
+		return res;
+	}
+	int ht_res = rte_hash_add_key_data(app_config->sessions, &session->key, session);
+	if (ht_res < 0) {
+		DOCA_LOG_ERR("Port %u: failed to add session %lu to hashtable: %d",
+		session->ingress_port, session->key, ht_res);
+		return ht_res;
 	}
 	return res;
 }
 
 doca_error_t
 inject_sample_sessions(
-	struct hairpin_mesh_app_config *app_config)
+	struct hairpin_mesh_app_config *app_config,
+	int n_sessions)
 {
-	struct session_entry session = {};
-
-	int n_sessions = 100;
 	for (int i=0; i<n_sessions; i++) {
-		session.key = i+1;
-		session.ingress_port = i % app_config->dpdk_config.port_config.nb_ports;
-		session.egress_port = (i + 1) % app_config->dpdk_config.port_config.nb_ports;
-		session.proto = (i < n_sessions/2) ? IPPROTO_UDP : IPPROTO_TCP;
-		session.src_ip = RTE_BE32(0x10101010 + i);
-		session.dst_ip = RTE_BE32(0x20101010 + i);
-		session.src_port = RTE_BE16(100 + i);
-		session.dst_port = RTE_BE16(200 + i);
-		session.snat_src_ip = RTE_BE32(0x30101010 + i);
-		session.snat_src_port = RTE_BE16(300 + i);
-		doca_error_t res = apply_session_flow(app_config, &session);
+		struct session_entry *session = rte_zmalloc("session", sizeof(struct session_entry), RTE_CACHE_LINE_SIZE);
+		session->key = i+1;
+		session->ingress_port = i % app_config->dpdk_config.port_config.nb_ports;
+		session->egress_port = (i + 1) % app_config->dpdk_config.port_config.nb_ports;
+		session->proto = (i < n_sessions/2) ? IPPROTO_UDP : IPPROTO_TCP;
+		session->src_ip = RTE_BE32(0x10101010 + i);
+		session->dst_ip = RTE_BE32(0x20101010 + i);
+		session->src_port = RTE_BE16(100 + i);
+		session->dst_port = RTE_BE16(200 + i);
+		session->snat_src_ip = RTE_BE32(0x30101010 + i);
+		session->snat_src_port = RTE_BE16(300 + i);
+		doca_error_t res = apply_session_flow(app_config, session);
 		if (res != DOCA_SUCCESS)
 			return res;
+		
+		char src_ip[INET_ADDRSTRLEN];
+		char dst_ip[INET_ADDRSTRLEN];
+		char snat_ip[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &session->src_ip, src_ip, INET_ADDRSTRLEN);
+		inet_ntop(AF_INET, &session->dst_ip, dst_ip, INET_ADDRSTRLEN);
+		inet_ntop(AF_INET, &session->snat_src_ip, snat_ip, INET_ADDRSTRLEN);
+		DOCA_LOG_WARN("port %d>%d: %s: %s:%d>%s:%d, src:=%s:%d",
+			session->ingress_port, session->egress_port,
+			session->proto == IPPROTO_UDP ? "UDP" : "TCP",
+			src_ip, RTE_BE16(session->src_port),
+			dst_ip, RTE_BE16(session->dst_port),
+			snat_ip, RTE_BE16(session->snat_src_port));
 	}
 	return DOCA_SUCCESS;
 }
@@ -373,26 +453,41 @@ create_session_pipe(
 		},
 	};
 	if (proto == IPPROTO_UDP) {
+		match.outer.l4_type_ext = DOCA_FLOW_L4_TYPE_EXT_UDP;
 		match.outer.udp.l4_port.src_port = UINT16_MAX;
 		match.outer.udp.l4_port.dst_port = UINT16_MAX;
 	} else {
+		match.outer.l4_type_ext = DOCA_FLOW_L4_TYPE_EXT_TCP;
 		match.outer.tcp.l4_port.src_port = UINT16_MAX;
 		match.outer.tcp.l4_port.dst_port = UINT16_MAX;
 	}
 
 	struct doca_flow_monitor mon = {
-		.flags = DOCA_FLOW_MONITOR_COUNT | DOCA_FLOW_MONITOR_AGING,
-		.aging = 5, // age timeout in seconds
+		// .flags = DOCA_FLOW_MONITOR_COUNT | DOCA_FLOW_MONITOR_AGING,
+		// .aging = 5, // age timeout in seconds
+		.flags = DOCA_FLOW_MONITOR_COUNT,
 	};
+
+	struct doca_flow_actions actions = {
+		.outer = {
+			.eth = {
+				.dst_mac = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff },
+				.type = DOCA_FLOW_ACTION_AUTO,
+			},
+		},
+	};
+	struct doca_flow_actions *actions_arr[] = { &actions };
 
 	struct doca_flow_pipe_cfg cfg = {
 		.attr = {
 			.name = name,
 			.type = DOCA_FLOW_PIPE_BASIC,
+			.nb_actions = 1,
 		},
 		.port = port_flows->doca_port,
 		.match = &match,
 		.monitor = &mon,
+		.actions = actions_arr,
 	};
 
 	struct doca_flow_fwd fwd = {
@@ -447,11 +542,16 @@ create_root_pipe_entry(
 	struct doca_flow_pipe_entry *entry = NULL;
 	doca_error_t res = doca_flow_pipe_control_add_entry(
 		queue, priority, port_flows->root_pipe, 
-		&match, NULL, NULL, NULL, NULL, &fwd, &entry);
-	
+		&match, NULL, NULL, NULL, NULL, &fwd, &entry);	
 	if (res != DOCA_SUCCESS) {
 		DOCA_LOG_ERR("Port %d: Failed to create root pipe %s entry: %s", 
 			port_id, proto==IPPROTO_UDP ? "UDP" : "TCP", doca_get_error_string(res));
+	}
+	res = doca_flow_entries_process(port_flows->doca_port, queue, default_entries_timeout_us, 0);
+	if (res != DOCA_SUCCESS) {
+		DOCA_LOG_ERR("Port %u: Failed to process root pipe entry: %s", 
+			port_id, doca_get_error_string(res));
+		return res;
 	}
 
 	return res;	
@@ -471,6 +571,7 @@ create_root_pipe(
 		.attr = {
 			.name = name,
 			.type = DOCA_FLOW_PIPE_CONTROL,
+			.is_root = true,
 		},
 		.port = port_flows->doca_port,
 	};
@@ -569,7 +670,7 @@ main(int argc, char **argv)
 	if (result != DOCA_SUCCESS)
 		return EXIT_FAILURE;
 	
-	result = inject_sample_sessions(&app_config);
+	result = inject_sample_sessions(&app_config, 10);
 	if (result != DOCA_SUCCESS)
 		return EXIT_FAILURE;
 
@@ -577,6 +678,35 @@ main(int argc, char **argv)
 	RTE_LCORE_FOREACH_WORKER(lcore_id) {
 		rte_eal_remote_launch(sample_lcore_func, &app_config, lcore_id);
 	}
+
+	size_t n_hits_prev = 0;
+	while (!force_quit)
+	{
+		size_t n_hits = 0;
+		uint32_t next = 0;
+		uint64_t *key = NULL;
+		struct session_entry *session = NULL;
+		while (rte_hash_iterate(app_config.sessions, (const void **)&key, (void **)&session, &next) != -ENOENT) {
+			if (!session || !session->flow_entry)
+				continue;
+			
+			struct doca_flow_query query_stats = {};
+			result = doca_flow_query_entry(session->flow_entry, &query_stats);
+			if (result != DOCA_SUCCESS) {
+				DOCA_LOG_ERR("Failed to query session %lu: %s",
+					session->key, doca_get_error_string(result));
+				break;
+			}
+			printf("Session %lu: hits: %lu\n", session->key, query_stats.total_pkts);
+			n_hits += query_stats.total_pkts;
+		}
+		printf("Total hairpin hits: %lu\n", n_hits);
+		if (n_hits != n_hits_prev) {
+			n_hits_prev = n_hits;
+		}
+		sleep(4);
+	}
+
 	RTE_LCORE_FOREACH_WORKER(lcore_id) {
 		rte_eal_wait_lcore(lcore_id);
 	}
